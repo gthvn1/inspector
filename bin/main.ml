f724aba40788284fd0b03406e35248e8926accf1
Guillaume <gthvn1@gmail.com>
2026-03-13 23:30:18 +0100

    Minor update

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 let () =
   let open Inspector in
-  if Array.length Sys.argv < 2 then (
+  if Array.length Sys.argv <> 2 then (
     Printf.eprintf "Usage: %s <logfile>\n" (Filename.basename Sys.argv.(0));
     exit 1);
 
