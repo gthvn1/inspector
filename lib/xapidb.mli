7e4f966f9ca8e9f384a442ff107ce243130bcaa4
Guillaume <guillaume.thouvenin@vates.tech>
2026-03-14 11:24:50 +0100

    Add odoc

--- lib/xapidb.mli
+++ lib/xapidb.mli
@@ -9,4 +9,9 @@ val size : t -> int
 (** [size t] returns the number of entries in the database *)
 
 val get_ref : t -> ref:string -> e list
+(** [get_ref t ~ref] returns the list of elements in the database for the given
+    reference. *)
+
 val elt_to_string : e -> string
+(** [elt_to_string elt] converts a database element to a human‑readable string.
+*)
