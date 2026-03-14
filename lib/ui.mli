53409d2adfc474d0bbd9953e865657d91c0e08a6
Guillaume <guillaume.thouvenin@vates.tech>
2026-03-14 15:51:24 +0100

    Indentation

--- lib/ui.mli
+++ lib/ui.mli
@@ -2,16 +2,21 @@ type t
 (** Keeps state related to the UI. *)
 
 val create : unit -> t
-(** [create ()] returns a UI state with truncation enabled by default and last command set to [None]. *)
+(** [create ()] returns a UI state with truncation enabled by default and last
+    command set to [None]. *)
 
 val get_last_cmd_opt : t -> string option
-(** [get_last_cmd_opt ui] returns the optional string that is the last command executed. *)
+(** [get_last_cmd_opt ui] returns the optional string that is the last command
+    executed. *)
 
 val set_last_cmd : string -> t -> t
-(** [set_last_cmd cmd ui] returns a UI state where the last command has been updated to [cmd]. *)
+(** [set_last_cmd cmd ui] returns a UI state where the last command has been
+    updated to [cmd]. *)
+
 val is_truncated : t -> bool
-(** [is_truncated ui] returns [true] if truncation is enabled, [false] otherwise. *)
+(** [is_truncated ui] returns [true] if truncation is enabled, [false]
+    otherwise. *)
 
 val switch_trunc : t -> t
-(** [switch_trunc ui] returns a new UI state where truncation has been toggled. *)
-
+(** [switch_trunc ui] returns a new UI state where truncation has been toggled.
+*)
