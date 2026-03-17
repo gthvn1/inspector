bb71861d2682829d0d035b0d27742d524a7982e9
Guillaume <gthvn1@gmail.com>
2026-03-17 22:29:02 +0100

    highlight uuid and opaqueref

--- lib/inspect.ml
+++ lib/inspect.ml
@@ -1,9 +1,26 @@
-let re = Re.Perl.compile_pat "OpaqueRef:([0-9a-f-]+)"
+let uuid_pattern =
+  "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
 
-let highlight line =
-  Re.replace re ~f:(fun g -> Style.yellow_text (Re.Group.get g 0)) line
+let re_ref = Re.Perl.compile_pat "OpaqueRef:([0-9a-f-]{36})"
+let re_uuid = Re.Perl.compile_pat uuid_pattern
 
 let find_opaqueref line =
-  match Re.all re line |> List.map (fun g -> Re.Group.get g 1) with
-  | [] -> [ "No OpaqueRef found" ]
+  match Re.all re_ref line |> List.map (fun g -> Re.Group.get g 1) with
+  | [] -> []
   | l -> l
+
+(* As uuid will path the substring of the opaqueref we first get OpaqueRef
+ if any, then extract UUID and remove them if they are OpaqueRef. *)
+let find_uuid line =
+  let refs = find_opaqueref line in
+  let all_uuids = Re.all re_uuid line |> List.map (fun g -> Re.Group.get g 1) in
+  List.filter (fun u -> not (List.mem u refs)) all_uuids
+
+let highlight line =
+  let refs = find_opaqueref line in
+  line
+  |> Re.replace re_ref ~f:(fun g -> Style.yellow_text (Re.Group.get g 0))
+  |> Re.replace re_uuid ~f:(fun g ->
+      (* Here, like fin_uuid, we are matching all UUID so we need to check if it is not an opaqueref *)
+      let u = Re.Group.get g 0 in
+      if List.mem u refs then u else Style.green_text u)
--- lib/style.ml
+++ lib/style.ml
@@ -1,8 +1,15 @@
 let bold = "\027[1m"
+let green = "\027[32m"
 let yellow = "\027[33m"
 let reverse = "\027[7m"
+
 let reset = "\027[0m"
+let reset_foreground = "\027[39m"
+
 let clear () = print_string "\027[2J\027[H"
-let bold_text s = bold ^ s ^ reset
-let yellow_text s = yellow ^ s ^ reset
+let bold_text s = bold ^ s ^ reset_foreground
+let green_text s = green ^ s ^ reset_foreground
+let yellow_text s = yellow ^ s ^ reset_foreground
+
+(* reverse is used for the whole line *)
 let reverse_text s = reverse ^ s ^ reset
